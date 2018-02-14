32e3cc9e6e552b73f320a15c34c00a74434b5902
Rokas Grigaravicius <grigroka@gmail.com>
2018-02-14 20:59:48 +0200

    Added ClearDB Addon

--- config/database.php
+++ config/database.php
@@ -1,5 +1,10 @@
 <?php
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
 return [
 
     /*
@@ -41,11 +46,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'db4free.net'),
+            'host' => env('DB_HOST', '$host'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'nfqtestapp1'),
-            'username' => env('DB_USERNAME', 'karvius'),
-            'password' => env('DB_PASSWORD', 'K59t15NqMETX'),
+            'database' => env('DB_DATABASE', '$database'),
+            'username' => env('DB_USERNAME', '$username'),
+            'password' => env('DB_PASSWORD', '$password'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
